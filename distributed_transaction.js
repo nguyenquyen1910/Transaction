215dab887cf4547ce5978d5c497ab4ce960e26ac
Nguyen Viet Quyen <140420436+nguyenquyen1910@users.noreply.github.com>
2025-03-25 23:59:20 +0700

    Add files via upload

---
+++ distributed_transaction.js
@@ -0,0 +1,111 @@
+import mongoose from "mongoose";
+
+// Kết nối đến database
+async function connectDatabase() {
+  const conn1 = await mongoose.createConnection(
+    "mongodb+srv://jrnguyen14:nHe1AqQMxPRUC8aU@transaction.vkh1w.mongodb.net/usersDB1?retryWrites=true&w=majority"
+  );
+
+  const conn2 = await mongoose.createConnection(
+    "mongodb+srv://jrnguyen14:nHe1AqQMxPRUC8aU@transaction.vkh1w.mongodb.net/usersDB2?retryWrites=true&w=majority"
+  );
+
+  return { conn1, conn2 };
+}
+
+// Định nghĩa Schema và Model
+const UserSchema = new mongoose.Schema({
+  name: String,
+  balance: Number,
+});
+
+// Thiết lập dữ liệu ban đầu
+async function setUpData() {
+  const { conn1, conn2 } = await connectDatabase();
+  const UserA = conn1.model("User", UserSchema);
+  const UserB = conn2.model("User", UserSchema);
+
+  // Xóa dữ liệu cũ và thêm dữ liệu mẫu trong usersDB1 và usersDB2
+  await Promise.all([
+    UserA.deleteMany({}),
+    UserB.deleteMany({})
+  ]);
+
+  const userA = await UserA.findOne({ name: "User_A" });
+    if (!userA) {
+      const newUserA = new UserA({ name: "User_A", balance: 1000 });
+      await newUserA.save();
+      console.log("User_A has been created");
+    }
+
+  const userB = await UserB.findOne({ name: "User_B" });
+  if (!userB) {
+    const newUserB = new UserB({ name: "User_B", balance: 500 });
+    await newUserB.save();
+    console.log("User_B has been created");
+  }
+  console.log("Successfully created users");
+
+  await conn1.close();
+  await conn2.close();
+}
+
+// Hàm thực hiện giao dịch phân tán
+async function runDistributedTransaction() {
+  let conn1, conn2;
+  let session1, session2;
+  const price = 50;
+  try {
+    // Kết nối tới hai database
+    ({ conn1, conn2 } = await connectDatabase());
+    const User1 = conn1.model("User", UserSchema);
+    const User2 = conn2.model("User", UserSchema);
+
+    // Bắt đầu session cho từng instance
+    session1 = await conn1.startSession();
+    session2 = await conn2.startSession();
+    await Promise.all([
+      session1.startTransaction(),
+      session2.startTransaction()
+    ]);
+
+    // Lấy userA và userB từ 2 database
+    const [userA, userB] = await Promise.all([
+      User1.findOne({ name: "User_A" }).session(session1),
+      User2.findOne({ name: "User_B" }).session(session2)
+    ]);
+
+    // Kiểm tra số dư của user1 và user2
+    if (!userA || userA.balance < price)
+      throw new Error("User1 does not have enough balance");
+    if (!userB) throw new Error("User2 does not exist");
+
+    // Thực hiện chuyển tiền từ userA qua userB
+    // Trừ tiền từ tài khoản người gửi trong userDB1
+    userA.balance -= price;
+    await userA.save({ session: session1 });
+
+    // Cộng tiền vào tài khoản người nhận trong userDB2
+    userB.balance += price;
+    await userB.save({ session: session2 });
+
+    // Commit cả hai giao dịch nếu thành công
+    await session1.commitTransaction();
+    await session2.commitTransaction();
+    console.log(`Successfully transaction! Price: ${price}`);
+  } catch (error) {
+    console.log("Failed transaction", error.message);
+    await session1.abortTransaction();
+    await session2.abortTransaction();
+  } finally {
+    if (session1) await session1.endSession();
+    if (session2) await session2.endSession();
+    if (conn1) await conn1.close();
+    if (conn2) await conn2.close();
+  }
+}
+
+// Chạy chương trình
+await setUpData();
+await runDistributedTransaction();
+console.log("Transaction complete.");
